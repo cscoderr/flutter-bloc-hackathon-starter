24ff4fe6b9b49c31eecea9f8437f4df0109bc17b
Idowu Tomiwa <tommydprogrammer@gmail.com>
2021-10-02 22:33:27 +0400

    initial commit

---
+++ lib/main_staging.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:flutter_bloc_hackathon_starter/app/app.dart';
+import 'package:flutter_bloc_hackathon_starter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
